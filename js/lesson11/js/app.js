91af5977f7de2cad3251e98e841eadcbc6ca85a9
iGuiDES <iosiguides@gmail.com>
2022-05-27 12:04:27 +0300

    update file app.js => optimized code

--- js/lesson11/js/app.js
+++ js/lesson11/js/app.js
@@ -3,40 +3,46 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
 
-function senderData() {
+document.getElementById('button').addEventListener('click', function (event) {
+    event.preventDefault();
+
     const users = {};
     const formData = document.forms['formData'];
 
     users.name = formData.name.value;
     users.age = formData.age.value;
 
-    localStorage.setItem("users", JSON.stringify(users));
+    if (formData.name.value !== '' && formData.age.value !== '') {
+        localStorage.setItem("users", JSON.stringify(users));
+    }
 
     formData.name.value = '';
     formData.age.value = '';
-}
-
-document.getElementById('button').addEventListener('click', senderData);
+});
 
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-function autoParameters() {
-    const formData = document.forms['auto'];
+document.getElementById('auto-data').addEventListener('click', function (event) {
+    event.preventDefault();
+
     const array = [];
     const auto = {};
+    const formData = document.forms['auto'];
 
     auto.model = formData.model.value;
     auto.type = formData.type.value;
     auto.volume = formData.volume.value;
 
     array.push(auto);
+    if (formData.model.value !== '' && formData.type.value !== '' && formData.volume.value !== '') {
+        localStorage.setItem('array', JSON.stringify(array));
+    }
 
-    localStorage.setItem('auto', JSON.stringify(array));
+    formData.model.value = '';
+    formData.type.value = '';
+    formData.volume.value = '';
 
     // const result = localStorage.getItem('auto');
     // console.log(JSON.parse(result));
-
-}
-
-document.getElementById('auto-data').addEventListener('click', autoParameters);
+});
