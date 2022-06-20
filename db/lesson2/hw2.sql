7594787814c8b0ab9bf01a49a683695bc49b518d
iGuiDES <iosiguides@gmail.com>
2022-06-20 10:30:25 +0300

    added DB lesson and update file README.md

--- db/lesson2/hw2.sql
+++ db/lesson2/hw2.sql
@@ -35,30 +35,88 @@ where Sum > 5000 order by Sum desc;
 
 # 9.Порахувати кількість клієнтів усіх відділень та лише львівських відділень.
 
+select
+    (select count(idClient) from client) as allClient,
+    (select count(idClient) from client where Department_idDepartment = 2 or Department_idDepartment = 5) as clientLviv;
 
+# select count(*) as sum_client_department, DepartmentCity
+# from client c join department d on d.idDepartment = c.Department_idDepartment
+# where DepartmentCity = 'lviv';
 
 # 10.Знайти кредити, які мають найбільшу суму для кожного клієнта окремо.
-#
+
+select FirstName, LastName, Sum, CreditState, Currency, City from client c join application a on c.idClient = a.Client_idClient
+where CreditState = 'Not returned' and Sum > 6000 and Currency = 'Gryvnia';
+
+select Client_idClient, max(Sum) from application group by Client_idClient;
+
 # 11. Визначити кількість заявок на крдеит для кожного клієнта.
-#
+
+select * from client c
+join application a on c.idClient = a.Client_idClient
+join department d on d.idDepartment = c.Department_idDepartment;
+
+select FirstName, LastName, count(Client_idClient) AS total_credits FROM application
+JOIN client a ON application.Client_idClient = a.idClient
+GROUP BY Client_idClient;
+
 # 12. Визначити найбільший та найменший кредити.
-#
+
+# select
+#     (select max(Sum) from application) as maxSumCreditClient,
+#     (select min(Sum) from application) as minSumCreditClient;
+
+select max(Sum) as MaxSumCredit, min(Sum) as MixSumCredit from application;
+
 # 13. Порахувати кількість кредитів для клієнтів,які мають вищу освіту.
-#
+
+select idClient, FirstName, LastName, count(Client_idClient) as TotalCredits from application
+join client a on application.Client_idClient = a.idClient
+where Education = 'high' group by Client_idClient;
+
 # 14. Вивести дані про клієнта, в якого середня сума кредитів найвища.
-#
+
+select idClient, FirstName, LastName, avg(Sum) as TotalSumCredit from application a
+join client c on c.idClient = a.Client_idClient
+where idClient group by Client_idClient order by TotalSumCredit DESC limit 1;
+
 # 15. Вивести відділення, яке видало в кредити найбільше грошей
-#
+
+select idDepartment, DepartmentCity, sum(Sum) as TotalSum from department d
+join client c on d.idDepartment = c.Department_idDepartment
+join application a on c.idClient = a.Client_idClient
+group by idDepartment, DepartmentCity order by TotalSum desc limit 1;
+
 # 16. Вивести відділення, яке видало найбільший кредит.
-#
+
+select Client_idClient, max(Sum) as sum from application a
+join client c on c.idClient = a.Client_idClient
+join department d on d.idDepartment = c.Department_idDepartment
+group by Client_idClient order by sum desc limit 1;
+
 # 17. Усім клієнтам, які мають вищу освіту, встановити усі їхні кредити у розмірі 6000 грн.
-#
+
+update application
+    join client c on c.idClient = application.Client_idClient
+    set Sum = 6000, Currency = 'Gryvnia'
+    where Education = 'high';
+
 # 18. Усіх клієнтів київських відділень пересилити до Києва.
-#
+
+update client set City = 'Kyiv'
+where Department_idDepartment = 1 or Department_idDepartment = 4;
+
+select * from client;
+
 # 19. Видалити усі кредити, які є повернені.
-#
+
+delete from application
+where CreditState = 'Returned';
+
 # 20. Видалити кредити клієнтів, в яких друга літера прізвища є голосною.
-#
+
+
+
 # 21.Знайти львівські відділення, які видали кредитів на загальну суму більше ніж 5000
 #
 # 22.Знайти клієнтів, які повністю погасили кредити на суму більше ніж 5000
